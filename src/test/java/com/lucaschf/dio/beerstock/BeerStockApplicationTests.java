82ce303c3b025a866420b9c646cfe218c3603ccf
lucaschfonseca <lucaschfonseca@gmail.com>
2021-06-26 14:20:51 -0300

    Developed service tests.

--- src/test/java/com/lucaschf/dio/beerstock/BeerStockApplicationTests.java
+++ src/test/java/com/lucaschf/dio/beerstock/BeerStockApplicationTests.java
@@ -1,10 +1,10 @@
-package com.lucaschf.dio.beerstock.beerstock;
+package com.lucaschf.dio.beerstock;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BeerstockApplicationTests {
+class BeerStockApplicationTests {
 
 	@Test
 	void contextLoads() {
